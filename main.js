f5803ab6c09f0ce72b41d42a567a0ccb25f5c91d
unknown <jorgesantanadev@gmail.com>
2021-01-14 11:40:32 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,28 @@
+
+
+ 
+function sorteio() {
+    let num = document.getElementById("txt");
+    
+    
+    
+    var getRandom = Math.floor((Math.random() * 100 ) + 1)
+
+  
+  
+    
+    num.innerHTML = `Numero Sorteado: ${getRandom}` 
+    
+    return getRandom;
+
+    
+
+    
+      
+    
+}
+
+
+
+
+
